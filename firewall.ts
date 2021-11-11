3fce87052a8e53de3f9174ad60cd413c1fa4eea1
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2021-11-11 22:41:12 +0100

    Remove ssh access from firewall

--- firewall.ts
+++ firewall.ts
@@ -5,12 +5,4 @@ import * as pulumi from "@pulumi/pulumi";
 let config = new pulumi.Config();
 let ghRunnerName = config.require("ghRunnerName");
 
-const computeFirewall = new gcp.compute.Firewall(`${ghRunnerName}-firewall`, {
-    network: network.id,
-    allows: [{
-        protocol: "tcp",
-        ports: [ "22" ],
-    }],
-});
-
-export const firewall = computeFirewall;
+export const firewall = new gcp.compute.Firewall(`${ghRunnerName}-firewall`, { network: network.id });
