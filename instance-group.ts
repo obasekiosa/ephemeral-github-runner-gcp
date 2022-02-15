da0608fba7dd706649b70cd62ed8a3bbdc025ebe
Ivan Pavlovic <pavlovic.ivan.26@gmail.com>
2022-02-15 17:21:44 +0100

    Remove comments

--- instance-group.ts
+++ instance-group.ts
@@ -7,7 +7,6 @@ const ghRunnerName = config.require("ghRunnerName");
 
 export const instanceGroup = new gcp.compute.InstanceGroupManager(`${ghRunnerName}-instance-group`, {
     baseInstanceName: ghRunnerName,
-    // zone: config.require("zone"),
     versions: [{
         instanceTemplate: instanceTemplate.id,
     }],
