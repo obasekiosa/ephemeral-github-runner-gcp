63f54afdc2d71cc72284c7504ecb2e6c7260f779
Osakpolor Obaseki <obasekiosa@gmail.com>
2022-04-12 15:17:49 +0100

    Export resourced identifaction values

--- aws/index.ts
+++ aws/index.ts
@@ -2,5 +2,12 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-import { FleetConfigureation} from "./instance-template";
+import { RunnerAutoScaleGroup } from "./instance";
+import { RunnerTemplate } from "./instance-template";
+import {group} from "./network";
 
+export const subnetId = group.id;
+export const Vpc = group.vpcId;
+
+export const template = RunnerTemplate.id;
+export const asg = RunnerAutoScaleGroup.id;
