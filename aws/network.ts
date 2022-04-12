38a479e50df68462e111a650dab7a5bc8b1c1317
Osakpolor Obaseki <obasekiosa@gmail.com>
2022-04-12 15:16:20 +0100

    Setup aws network configuration

--- aws/network.ts
+++ aws/network.ts
@@ -1,15 +1,14 @@
 import * as aws from "@pulumi/aws";
-import * as pulumi from "@pulumi/pulumi";
 
 
-export const group = new aws.ec2.SecurityGroup("sec-group-name", {
+export const group = new aws.ec2.SecurityGroup("ghrunner-security-group", {
     ingress: [
         {protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
-    ]
-});
-
-export const subnet = new aws.ec2.Subnet("subnet-name", {
-    // vpcId: selected.then(selected => selected.vpcId),
-    vpcId: group.vpcId,
-    cidrBlock: "0.0.0.0/0",
+    ],
+    egress: [
+        { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
+    ],
+    tags: {
+        "Name":"ghrunner"
+    },
 });
